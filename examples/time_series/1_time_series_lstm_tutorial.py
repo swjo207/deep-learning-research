1024cf7387cf99d673fb987d1fc08117fee747b0
swjo207 <swjo207@gmail.com>
2018-08-15 01:17:07 +0900

    simple LSTM time series analysis

---
+++ examples/time_series/1_time_series_lstm_tutorial.py
@@ -0,0 +1,319 @@
+"""
+this is aimed to be a tutorial for beginner in DL and time series analysis
+
+data source :
+    http://archive.ics.uci.edu/ml/datasets/Individual+household+electric+power+consumption
+    Measurements of electric power consumption in one household with a one-minute sampling rate over a period of almost 4 years.
+    Different electrical quantities and some sub-metering values are available.
+
+1.date: Date in format dd/mm/yyyy
+2.time: time in format hh:mm:ss
+3.global_active_power: household global minute-averaged active power (in kilowatt)
+4.global_reactive_power: household global minute-averaged reactive power (in kilowatt)
+5.voltage: minute-averaged voltage (in volt)
+6.global_intensity: household global minute-averaged current intensity (in ampere)
+7.sub_metering_1: energy sub-metering No. 1 (in watt-hour of active energy). It corresponds to the kitchen, containing mainly a dishwasher, an oven and a microwave (hot plates are not electric but gas powered).
+8.sub_metering_2: energy sub-metering No. 2 (in watt-hour of active energy). It corresponds to the laundry room, containing a washing-machine, a tumble-drier, a refrigerator and a light.
+9.sub_metering_3: energy sub-metering No. 3 (in watt-hour of active energy). It corresponds to an electric water-heater and an air-conditioner.
+
+
+model:
+    LSTM
+"""
+
+import sys
+import numpy as np # linear algebra
+from scipy.stats import randint
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv), data manipulation as in SQL
+import matplotlib.pyplot as plt # this is used for the plot the graph
+import seaborn as sns # used for plot interactive graph.
+from sklearn.model_selection import train_test_split # to split the data into two parts
+from sklearn.cross_validation import KFold # use for cross validation
+from sklearn.preprocessing import StandardScaler # for normalization
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.pipeline import Pipeline # pipeline making
+from sklearn.model_selection import cross_val_score
+from sklearn.feature_selection import SelectFromModel
+from sklearn import metrics # for the check the error and accuracy of the model
+from sklearn.metrics import mean_squared_error,r2_score
+import wget
+
+## for Deep-learing:
+import keras
+from keras.layers import Dense
+from keras.models import Sequential
+from keras.layers.core import Activation
+from keras.utils import to_categorical
+from keras.optimizers import SGD
+from keras.callbacks import EarlyStopping
+from keras.utils import np_utils
+import itertools
+from keras.layers import LSTM
+from keras.layers.convolutional import Conv1D
+from keras.layers.convolutional import MaxPooling1D
+from keras.layers import Dropout
+
+from io import BytesIO
+from zipfile import ZipFile
+from urllib.request import urlopen
+from pathlib import Path
+
+url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip'
+
+if Path('./data/household_power_consumption.txt').is_file():
+    print('file exists')
+else:
+    print('file downloading...')
+    resp = urlopen(url)
+    zipfile = ZipFile(BytesIO(resp.read()))
+    for name in zipfile.namelist():
+        outpath = "./data"
+        zipfile.extract(name, outpath)
+    print('downloading is completed.')
+
+df = pd.read_csv('./data/household_power_consumption.txt', sep=';',
+                 parse_dates={'dt' : ['Date', 'Time']}, infer_datetime_format=True,
+                 low_memory=False, na_values=['nan','?'], index_col='dt')
+#print(df.head())
+#print(df.info())
+#print(df.dtypes)
+#print(df.shape)
+#print(df.describe())
+#print(df.columns)
+
+drop_list=[]
+for i in range(0,7):
+    if not df.iloc[:,i].notnull().all():
+        drop_list.append(i)
+#print(drop_list)
+
+for i in range(0,7):
+    df.iloc[:,i] = df.iloc[:,i].fillna(df.iloc[:,i].mean())
+
+#print(df.isnull().sum())
+
+"""
+# 일간 단위로 샘플링해서 시각화
+r = df.Global_active_power.resample('D').agg(['mean', 'sum'])
+r.plot(subplots=True, title='Global_active_power resampled over day')
+plt.show()
+
+r = df.Global_intensity.resample('D').agg(['mean', 'std'])
+r.plot(subplots = True, title='Global_intensity resampled over day')
+plt.show()
+
+# 월 단위로 샘플링해서 시각화 (바차트)
+df['Global_active_power'].resample('M').mean().plot(kind='bar')
+plt.xticks(rotation=60)
+plt.ylabel('Global_active_power')
+plt.title('Global_active_power per month (averaged over month)')
+plt.show()
+
+# 분기 단위로 샘플링해서 시각화 (바차트)
+df['Global_active_power'].resample('Q').mean().plot(kind='bar')
+plt.xticks(rotation=60)
+plt.ylabel('Global_active_power')
+plt.title('Global_active_power per quarter (averaged over quarter)')
+plt.show()
+
+# 전압에 대해 월별 샘플링해서 시각화
+df['Voltage'].resample('M').mean().plot(kind='bar', color='red')
+plt.xticks(rotation=60)
+plt.ylabel('Voltage')
+plt.title('Voltage per quarter (summed over quarter)')
+plt.show()
+
+# sub_metering_1 에 대해 월별 샘플링해서 시각화
+df['Sub_metering_1'].resample('M').mean().plot(kind='bar', color='brown')
+plt.xticks(rotation=60)
+plt.ylabel('Sub_metering_1')
+plt.title('Sub_metering_1 per quarter (summed over quarter)')
+plt.show()
+"""
+
+
+"""
+# Below I compare the mean of different featuresresampled over day.
+# specify columns to plot
+cols = [0, 1, 2, 3, 5, 6]
+i = 1
+groups=cols
+values = df.resample('D').mean().values
+# plot each column
+plt.figure(figsize=(15, 10))
+for group in groups:
+	plt.subplot(len(cols), 1, i)
+	plt.plot(values[:, group])
+	plt.title(df.columns[group], y=0.75, loc='right')
+	i += 1
+plt.show()
+
+## resampling over week and computing mean
+df.Global_reactive_power.resample('W').mean().plot(color='y', legend=True)
+df.Global_active_power.resample('W').mean().plot(color='r', legend=True)
+df.Sub_metering_1.resample('W').mean().plot(color='b', legend=True)
+df.Global_intensity.resample('W').mean().plot(color='g', legend=True)
+plt.show()
+
+# Below I show hist plot of the mean of different feature resampled over month
+df.Global_active_power.resample('M').mean().plot(kind='hist', color='r', legend=True )
+df.Global_reactive_power.resample('M').mean().plot(kind='hist',color='b', legend=True)
+#df.Voltage.resample('M').sum().plot(kind='hist',color='g', legend=True)
+df.Global_intensity.resample('M').mean().plot(kind='hist', color='g', legend=True)
+df.Sub_metering_1.resample('M').mean().plot(kind='hist', color='y', legend=True)
+plt.show()
+
+## The correlations between 'Global_intensity', 'Global_active_power'
+data_returns = df.pct_change()
+sns.jointplot(x='Global_intensity', y='Global_active_power', data=data_returns)
+plt.show()
+
+sns.jointplot(x='Voltage', y='Global_active_power', data=data_returns)
+plt.show()
+
+"""
+
+"""
+# Correlations among columns
+plt.matshow(df.corr(method='spearman'),vmax=1,vmin=-1,cmap='PRGn')
+plt.title('without resampling', size=15)
+plt.colorbar()
+plt.show()
+
+# Correlations of mean of features resampled over months
+plt.matshow(df.resample('M').mean().corr(method='spearman'),vmax=1,vmin=-1,cmap='PRGn')
+plt.title('resampled over month', size=15)
+plt.colorbar()
+plt.margins(0.02)
+plt.matshow(df.resample('A').mean().corr(method='spearman'),vmax=1,vmin=-1,cmap='PRGn')
+plt.title('resampled over year', size=15)
+plt.colorbar()
+plt.show()
+"""
+
+
+def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+    n_vars = 1 if type(data) is list else data.shape[1]
+    dff = pd.DataFrame(data)
+    cols, names = list(), list()
+    # input sequence (t-n, ... t-1)
+    for i in range(n_in, 0, -1):
+        cols.append(dff.shift(i))
+        names += [('var%d(t-%d)' % (j + 1, i)) for j in range(n_vars)]
+    # forecast sequence (t, t+1, ... t+n)
+    for i in range(0, n_out):
+        cols.append(dff.shift(-i))
+        if i == 0:
+            names += [('var%d(t)' % (j + 1)) for j in range(n_vars)]
+        else:
+            names += [('var%d(t+%d)' % (j + 1, i)) for j in range(n_vars)]
+    # put it all together
+    agg = pd.concat(cols, axis=1)
+    agg.columns = names
+    # drop rows with NaN values
+    if dropnan:
+        agg.dropna(inplace=True)
+    return agg
+
+
+## 분단위 데이터를 시간 단위로 바꾼다. (데이터 구조를 크게 변하지 않으면서 연산 시간을 줄이기 위해)
+df_resample = df.resample('h').mean()
+print(df_resample.shape)
+
+## 데이터 정규화 : [0,1] 범위 안에 오도록 데이터를 변환 시킨다.
+values = df_resample.values
+
+## full data without resampling
+#values = df.values
+
+# integer encode direction ensure all data is float
+values = values.astype('float32')
+# normalize features
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled = scaler.fit_transform(values)
+# frame as supervised learning
+reframed = series_to_supervised(scaled, 1, 1)
+
+# drop columns we don't want to predict
+reframed.drop(reframed.columns[[8,9,10,11,12,13]], axis=1, inplace=True)
+print(reframed.head())
+
+
+# 데이터를 학습용과 검증용으로 분리
+# split into train and test sets
+values = reframed.values
+
+n_train_time = 365*24*3
+train = values[:n_train_time, :]
+test = values[n_train_time:, :]
+##test = values[n_train_time:n_test_time, :]
+# split into input and outputs
+train_X, train_y = train[:, :-1], train[:, -1]
+test_X, test_y = test[:, :-1], test[:, -1]
+# reshape input to be 3D [samples, timesteps, features]
+train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
+test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
+print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
+# We reshaped the input into the 3D format as expected by LSTMs, namely [samples, timesteps, features].
+
+# 모델 아키텍처
+"""
+1) LSTM with 100 neurons in the first visible layer
+3) dropout 20%
+4) 1 neuron in the output layer for predicting Global_active_power.
+5) The input shape will be 1 time step with 7 features.
+6) I use the Mean Absolute Error (MAE) loss function and the efficient Adam version of stochastic gradient descent.
+7) The model will be fit for 20 training epochs with a batch size of 70.
+"""
+
+model = Sequential()
+model.add(LSTM(input_shape=(train_X.shape[1], train_X.shape[2]),units=100, return_sequences=True ))
+model.add(Dropout(0.2))
+model.add(LSTM(units=70, return_sequences=False))
+model.add(Dropout(0.2))
+model.add(Dense(1))
+model.add(Activation("linear"))
+model.compile(loss='mean_squared_error', optimizer='rmsprop')
+
+
+
+# fit network
+history = model.fit(train_X, train_y, epochs=30, batch_size=70, validation_data=(test_X, test_y), verbose=2, shuffle=False)
+
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper right')
+plt.show()
+
+# make a prediction
+yhat = model.predict(test_X)
+test_X = test_X.reshape((test_X.shape[0], 7))
+# invert scaling for forecast
+inv_yhat = np.concatenate((yhat, test_X[:, -6:]), axis=1)
+inv_yhat = scaler.inverse_transform(inv_yhat)
+inv_yhat = inv_yhat[:,0]
+# invert scaling for actual
+test_y = test_y.reshape((len(test_y), 1))
+inv_y = np.concatenate((test_y, test_X[:, -6:]), axis=1)
+inv_y = scaler.inverse_transform(inv_y)
+inv_y = inv_y[:,0]
+# calculate RMSE
+rmse = np.sqrt(mean_squared_error(inv_y, inv_yhat))
+print('Test RMSE: %.3f' % rmse)
+
+
+## 성능 튜닝하기
+## time steps, every step is one hour (you can easily convert the time step to the actual time index)
+## for a demonstration purpose, I only compare the predictions in 200 hours.
+
+aa=[x for x in range(4000)]
+plt.plot(aa, inv_y[:4000], marker='.', label="actual")
+plt.plot(aa, inv_yhat[:4000], 'r', label="prediction")
+plt.ylabel('Global_active_power', size=15)
+plt.xlabel('Time step', size=15)
+plt.legend(fontsize=15)
+plt.show()
